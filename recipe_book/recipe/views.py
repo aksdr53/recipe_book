e8079cafe407e0a695d01d525f18994bcfd675e7
unknown <ingaksdr@gmail.com>
2024-01-28 20:49:45 +0300

    add transactions

--- recipe_book/recipe/views.py
+++ recipe_book/recipe/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.db import transaction
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, renderer_classes
@@ -13,25 +14,27 @@ def add_product_to_recipe(request):
     product_id = request.GET.get('product_id')
     ingredient = get_object_or_404(Ingredient, id=product_id)
     weight = request.GET.get('weight')
-    product, created = IngredientsAmount.objects.update_or_create(
-        recipe=recipe,
-        ingredient=ingredient,
-        defaults={'weight': weight}
-    )
-    if created:
-        return Response(status=status.HTTP_201_CREATED)
-    return Response(status=status.HTTP_200_OK)
+    with transaction.atomic():
+        product, created = IngredientsAmount.objects.update_or_create(
+            recipe=recipe,
+            ingredient=ingredient,
+            defaults={'weight': weight}
+        )
+        if created:
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(('GET',))
 def cook_recipe(request):
     recipe_id = request.GET.get('recipe_id')
     recipe = get_object_or_404(Recipe, id=recipe_id)
     ingredients = recipe.ingredients_amount_in_recipe.all()
-    for ingredient in ingredients:
-        count = ingredient.ingredient.use_count
-        ingredient.ingredient.use_count = count + 1
-        ingredient.ingredient.save()
-    return Response(status=status.HTTP_200_OK)
+    with transaction.atomic():
+        for ingredient in ingredients:
+            count = ingredient.ingredient.use_count
+            ingredient.ingredient.use_count = count + 1
+            ingredient.ingredient.save()
+        return Response(status=status.HTTP_200_OK)
 
 
 
